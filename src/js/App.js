b800c00620be235d603de33c8fc92050572a1138
Djosc <davidjoscaldwell@gmail.com>
2022-03-25 22:53:48 -0400

    Use dummy satellite data

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CzmlDataSource, Viewer } from 'resium';
 
-import { iss } from '../data/czml';
+import { satellites } from '../data/czml';
 
 function App() {
+	// const [data, setData] = useState('');
+
+	useEffect(() => {
+		fetch('https://celestrak.com/NORAD/elements/gp.php?CATNR=25544&FORMAT=TLE')
+			.then((response) => response.text())
+			.then((data) => console.log(data));
+	}, []);
+
 	return (
 		<Viewer full>
-			<CzmlDataSource data={iss} />
+			<CzmlDataSource data={satellites} />
 		</Viewer>
 	);
 }
