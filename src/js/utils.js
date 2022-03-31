01a9504df0a93b1bf42e7f32adfb9611d8f6cbdd
Djosc <davidjoscaldwell@gmail.com>
2022-03-30 22:40:05 -0400

    Convert e notation to percent

--- src/js/utils.js
+++ src/js/utils.js
@@ -119,34 +119,6 @@ const getOrbitalPeriod = (satrec) => {
 	return period;
 };
 
-export const mapCollisionDataToObjects = async (collisionData) => {
-	var collisionsArr = [];
-
-	for (let i = 0, j = 0; i < collisionData.length; i += 14, j++) {
-		let newArr = collisionData.slice(i, i + 14);
-
-		collisionsArr.push({
-			COLLISION_RANKING: j,
-			NORAD_CAT_ID_1: newArr[1],
-			NAME_1: newArr[2],
-			DAYS_SINCE_EPOCH_1: newArr[3],
-			MAX_PROBABILITY: newArr[4],
-			DILUTION_THRESHOLD_KM: newArr[5],
-			MIN_RANGE_KM: newArr[6],
-			REL_VELOCITY_KM_SEC: newArr[7],
-			NORAD_CAT_ID_2: newArr[8],
-			NAME_2: newArr[9],
-			DAYS_SINCE_EPOCH_2: newArr[10],
-			START_UTC: newArr[11],
-			CLOSEST_APPROACH_UTC: newArr[12],
-			STOP_UTC: newArr[13],
-		});
-	}
-
-	// console.log(collisionsArr);
-	return collisionsArr;
-};
-
 export const setOrbits = async (collisionObjects) => {
 	var orbitsArr = [];
 	var tleArr = [];
@@ -183,4 +155,40 @@ export const setOrbits = async (collisionObjects) => {
 	});
 
 	return orbits;
+};
+
+export const mapCollisionDataToObjects = async (collisionData) => {
+	var collisionsArr = [];
+
+	for (let i = 0, j = 0; i < collisionData.length; i += 14, j++) {
+		let newArr = collisionData.slice(i, i + 14);
+
+		let probPercent = eNotationToPercent(newArr[4]);
+
+		collisionsArr.push({
+			COLLISION_RANKING: j,
+			NORAD_CAT_ID_1: newArr[1],
+			NAME_1: newArr[2],
+			DAYS_SINCE_EPOCH_1: newArr[3],
+			MAX_PROBABILITY: probPercent,
+			DILUTION_THRESHOLD_KM: newArr[5],
+			MIN_RANGE_KM: newArr[6],
+			REL_VELOCITY_KM_SEC: newArr[7],
+			NORAD_CAT_ID_2: newArr[8],
+			NAME_2: newArr[9],
+			DAYS_SINCE_EPOCH_2: newArr[10],
+			START_UTC: newArr[11],
+			CLOSEST_APPROACH_UTC: newArr[12],
+			STOP_UTC: newArr[13],
+		});
+	}
+
+	// console.log(collisionsArr);
+	return collisionsArr;
+};
+
+const eNotationToPercent = (maxProbability) => {
+	let num = Number(maxProbability);
+	let string = (num * 100).toFixed(2).toString();
+	return (string += '%');
 };
